8a60dd69db40b829f3caaf2a521cb6f9d631edee
Elias Fleckenstein <eliasfleckenstein@web.de>
2022-03-06 16:24:18 +0100

    Add info command

--- basic.js
+++ basic.js
@@ -1,6 +1,14 @@
 const common = require("./common.js")
 
 module.exports = {
+	info: {
+		help: "Display info about furrybot",
+		func: msg => msg.reply("**furrybot** _by Elias Fleckenstein_\n\n"
+			+ "Furrybot for Discord: <https://github.com/EliasFleckenstein03/furrybot-discord>\n"
+			+ "Original Furrybot Dragonfire CSM: <https://github.com/EliasFleckenstein03/furrybot>\n"
+			+ "Custom Google Images API: <https://www.npmjs.com/package/free-google-images>"
+		)
+	},
 	help: {
 		params: "[<command>]",
 		help: "Display help for a commands or show list of available commands",
