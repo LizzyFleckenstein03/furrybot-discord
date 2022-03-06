54112e2795486dd4c34ae448404bc34b914c8409
Elias Fleckenstein <eliasfleckenstein@web.de>
2022-03-06 20:50:38 +0100

    Complain about voice channel before resolving url

--- music.js
+++ music.js
@@ -5,6 +5,11 @@ const youtubeSearchApi = require("youtube-search-api")
 module.exports = {
 	play: {
 		func: async (msg, urlArr) => {
+			const channel = msg.member.voice.channel
+
+			if (! channel)
+				return msg.reply("Join a voice channel you fucking moron")
+
 			let url = urlArr.join(" ")
 
 			try {
@@ -13,11 +18,6 @@ module.exports = {
 				url = "https://youtube.com/watch?v=" + (await youtubeSearchApi.GetListByKeyword(url, false, 1)).items[0].id
 				msg.reply("Playing this: " + url)
 			}
-
-			const channel = msg.member.voice.channel
-
-			if (! channel)
-				return msg.reply("Join a voice channel you fucking moron")
 
 			const conn = voice.joinVoiceChannel({
 				channelId: channel.id,
